daa22372bcb3a975b2e6b95ca1bda9f18f45017e
carotina99 <37614664+carotina99@users.noreply.github.com>
2018-03-21 12:38:15 +0100

    Add files via upload

---
+++ ToDo.java
@@ -0,0 +1,33 @@
+
+
+public class ToDo
+{
+    
+
+    
+    public ToDo()
+    {
+       ;
+    }
+
+    
+    public String elencaProgetto()
+    {
+        String a =new a [];
+    }
+    
+    public Object selezionaProgetto(String denominazione)
+    {
+        ;
+    }
+    
+    public Object creaProgetto(String denominazione)
+    {
+      Object o = new Object();
+    }
+    
+    public elimina progetto(String denominazione)
+    {
+         ;
+    }
+}
